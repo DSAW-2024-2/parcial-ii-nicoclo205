c817ac159cb53bfdaed9215e114e96a343448ae1
nicoclo205 <148281570+nicoclo205@users.noreply.github.com>
2024-10-04 08:11:53 -0500

    Handling not found routes

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.use(express.json());
 app.use('/login', require('./routes/login'));
 app.use('/weather', require('./routes/weather'));
 
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Not found' });
+    });
+
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
     });
