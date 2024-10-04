74d067da5010b186939fe937f8dae89dc9119588
nicoclo205 <148281570+nicoclo205@users.noreply.github.com>
2024-10-04 07:38:22 -0500

    applying the routes in the index.js

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const jwt = require('jsonwebtoken');
+const fetch = require('node-fetch');
+require('dotenv').config();
 
+app.use(express.json());
+app.use('/login', require('./routes/login'));
+app.use('/weather', require('./routes/weather'));
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+    });
