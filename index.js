8b0921b704b0c0a07243d56fed76de66eeacbbc0
nicoclo205 <148281570+nicoclo205@users.noreply.github.com>
2024-10-04 08:43:07 -0500

    changes in index.js

--- index.js
+++ index.js
@@ -5,28 +5,36 @@ const jwt = require('jsonwebtoken');
 const fetch = require('node-fetch');
 require('dotenv').config();
 
+const loginRouter = require('./routes/login');
+const weatherRouter = require('./routes/weather');
+
 app.use(express.json());
 
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
-    if (token == null) return res.sendStatus(401);
+    if (!token) return res.sendStatus(401); 
+
+    if (!process.env.ACCESS_TOKEN_SECRET) {
+        console.error("ACCESS_TOKEN_SECRET is not defined in environment variables.");
+        return res.sendStatus(500); 
+    }
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
+        if (err) return res.sendStatus(403); 
         req.user = user;
         next();
     });
 }
 
-app.use('/login', require('./routes/login'));
-app.use('/weather', require('./routes/weather'));
+app.use('/login', loginRouter);
+app.use('/weather', authenticateToken, weatherRouter); 
 
-app.use((req, res, next) => {
+app.use((req, res) => {
     res.status(404).json({ message: 'Not found' });
-    });
+});
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
-    });
+});
