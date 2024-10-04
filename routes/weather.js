1682e05e3d0dc825a173be0952a89331a7c7ec3c
nicoclo205 <148281570+nicoclo205@users.noreply.github.com>
2024-10-04 09:01:09 -0500

    changes in package.json and routes

--- routes/weather.js
+++ routes/weather.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-app.use(express.json());
-app.use('/weather', weatherRouter);
-
 router.get('/', async (req, res) => {
     try {
-        const { latitude, longitude } = req.query; // Obtenemos latitud y longitud de la query string
+        const { latitude, longitude } = req.query; 
         if (!latitude || !longitude) {
             return res.status(400).json({ message: 'Latitude and longitude are required' });
         }
@@ -14,11 +11,14 @@ router.get('/', async (req, res) => {
         const APIurl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`;
 
         const response = await fetch(APIurl);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
         const weatherData = await response.json();
 
         res.json(weatherData);
     } catch (error) {
-        res.status(500).json({ message: 'Error fetching weather data', error });
+        res.status(500).json({ message: 'Error fetching weather data', error: error.message });
     }
 });
 
