5f3994d72d0e4abd5e9eabbabda8ce4fbd3a6519
nicoclo205 <148281570+nicoclo205@users.noreply.github.com>
2024-10-04 08:25:30 -0500

    the authenticateToken function was placed from the weather route to the index

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const fetch = require('node-fetch');
 require('dotenv').config();
 
 app.use(express.json());
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
+
 app.use('/login', require('./routes/login'));
 app.use('/weather', require('./routes/weather'));
 
--- routes/weather.js
+++ routes/weather.js
@@ -6,19 +6,6 @@ const APIurl = `https://api.open-meteo.com/v1/forecast?latitude=${req.query.lati
 app.use(express.json());
 app.use('/weather', weatherRouter);
 
-function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-
-    if (token == null) return res.sendStatus(401);
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.user = user;
-        next();
-    });
-}
-
 router.get('/', authenticateToken, (req, res) => {
     const {latitude, longitude} = req.query;
 
